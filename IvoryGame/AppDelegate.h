80ba332334ba632a12162ef3f06965f8ba0cc28f
Miroslav Ivanov <miroslavivanov@miroslavs-mac.local>
2016-02-01 22:48:31 +0200

    Initial Commit

---
+++ IvoryGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IvoryGame
+//
+//  Created by Miroslav Ivanov on 2/1/16.
+//  Copyright © 2016 Miroslav Ivanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IvoryGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IvoryGame
+//
+//  Created by Miroslav Ivanov on 2/1/16.
+//  Copyright © 2016 Miroslav Ivanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
